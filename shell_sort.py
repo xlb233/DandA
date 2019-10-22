d72c37cfa6b673b809a0ebd6555bc72fb96606ff
xlb233 <121305596@qq.com>
2019-10-22 22:24:40 +0800

    增加希尔排序

--- shell_sort.py
+++ shell_sort.py
@@ -1,15 +1,15 @@
 def shell_sort(arr):
     n = len(arr)
     # 定义递增序列的间距，并逐步缩小间距直到1
-    gap = n / 2
+    gap = int(n / 3)
     while gap > 0:
         # 对递增序列进行插入排序
         for i in range(gap, n):
             j = i
-            while j >= gap and arr[j] < arr[j - 1]:
-                arr[j], arr[j - 1] = arr[j - 1], arr[j]
+            while j >= gap and arr[j] < arr[j - gap]:
+                arr[j], arr[j - gap] = arr[j - gap], arr[j]
                 j -= gap
-        gap /= 2
+        gap = int(gap / 3)
     return arr
 
 
