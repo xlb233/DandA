878541cc9a41128af76b8a027a200b749c9013df
xlb233 <121305596@qq.com>
2019-10-23 01:00:34 +0800

    归并排序的递归实现

---
+++ merge_sort.py
@@ -0,0 +1,29 @@
+# 分治，分和治
+# merge是和
+
+
+def merge(left, right):
+    result = []
+    while left and right:
+        if left[0] <= right[0]:
+            result.append(left.pop(0))
+        else:
+            result.append(right.pop(0))
+    if left:
+        result += left
+    if right:
+        result += right
+    return result
+
+
+# merge_sort是分
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    mid = len(arr) // 2
+    left = arr[:mid]
+    right = arr[mid:]
+
+    left = merge_sort(left)
+    right = merge_sort(right)
+    return merge(left, right)
