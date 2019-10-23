966f281148f77523359a07a056cc6dfaf007436f
xlb233 <121305596@qq.com>
2019-10-23 11:18:16 +0800

    冒泡排序

---
+++ bubble_sort.py
@@ -0,0 +1,12 @@
+import random
+
+
+def bubble_sort(arr):
+    for i in range(len(arr)):
+        for j in range(i, len(arr)):
+            if arr[i] > arr[j]:
+                arr[i], arr[j] = arr[j], arr[i]
+    return arr
+
+
+print(bubble_sort([random.random()*5000 for _ in range(100)]))
