e0981d46cd0b9e18f2617f2e4c4edc20d63fd747
xlb233 <121305596@qq.com>
2019-10-24 19:59:04 +0800

    将归并排序的实现改得不那么吃python语法优势

--- merge_sort.py
+++ merge_sort.py
@@ -5,15 +5,20 @@ import random
 
 def merge(left, right):
     result = []
-    while left and right:
-        if left[0] <= right[0]:
-            result.append(left.pop(0))
+    i = j = 0
+    while i < len(left) and j < len(right):
+        if left[i] <= right[j]:
+            result.append(left[i])
+            i += 1
         else:
-            result.append(right.pop(0))
-    if left:
-        result += left
-    if right:
-        result += right
+            result.append(right[j])
+            j += 1
+    if i < len(left):
+        for obj in left[i:]:
+            result.append(obj)
+    if j < len(right):
+        for obj in right[j:]:
+            result.append(obj)
     return result
 
 
