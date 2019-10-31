f19cf54b7ea1e4f8de585aa4de4ead382b8bf508
xlb233 <121305596@qq.com>
2019-10-31 21:47:26 +0800

    《剑指offer》双指针法找出链表倒数第k个元素

--- find_kth_to_tail.py
+++ find_kth_to_tail.py
@@ -8,12 +8,15 @@ class Node:
         self.next = nxt
 
 
-
 def find_kth_to_tail(head, k):
     fast = head
     slow = head
     if type(head) is not Node or k <= 0:
         return None
+    # 这里为何快指针先走k-1呢
+    # 假设链表长度为n，则倒数第k个元素即为正数的第n-k+1个元素
+    # 如果需要慢指针移到最后时快指针正好到达要找的元素
+    # 也就是需要相距n-（n-k+1）= k-1
     for _ in range(k - 1):
         if fast.next:
             fast = fast.next
