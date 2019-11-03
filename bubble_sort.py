fb79e140ccf50a0f4ef299827fdc28c13e97f278
xlb233 <121305596@qq.com>
2019-11-03 22:35:01 +0800

    二分查找

--- bubble_sort.py
+++ bubble_sort.py
@@ -3,7 +3,7 @@ import random
 
 def bubble_sort(arr):
     for i in range(len(arr)):
-        for j in range(i, len(arr)):
+        for j in range(i+1, len(arr)):
             if arr[i] > arr[j]:
                 arr[i], arr[j] = arr[j], arr[i]
     return arr
